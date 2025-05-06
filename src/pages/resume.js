9378c1f799a26381b46145af55f6f6e5cc07e5c0
Raghav Chhabra <91244493+erraghavchhabra@users.noreply.github.com>
2025-05-06 17:33:51 +0530

    code update 2

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import demoImg from "../assets/img/demo.jpg";
+import googleLogo from "../assets/icons/google.png";
+import BlanoiaLogo from "../assets/icons/Blanoia-Logo-02-1024-310x310.png";
+import docklandsLogo from "../assets/icons/docklands-media-logo-1.png";
+import DuisburgEssen from "../assets/icons/Uni-duisburg-essen-logo-2022.jpg";
+import IDEOULogo from "../assets/icons/IDEO_u.jpg";
+import SaudiAramcoLogo from "../assets/icons//Saudi-Aramco-Logo-old.png";
+
 const Resume = () => {
   return (
     <div>
-
       <div className="container-fluid entry-content">
         <div className="row">
           <div className="col-lg-12">
@@ -14,28 +18,55 @@ const Resume = () => {
               <div class="timeline-item">
                 <h5 class="item-period ">2021 - 2024</h5>
                 <span class="item-company">Riyadh, Saudi Arabia</span>
-                <h4 class="item-title">Imam Abdulaziz Royal Reserve Development Authority</h4>
+                <h4 class="item-title">
+                  Imam Abdulaziz Royal Reserve Development Authority
+                </h4>
                 <div class="text">
-                  Successfully produced over 300 pieces of content annually, driving a significant increase in social media engagement that grew the follower base to over 20,000. Supervised exhibitions, ensuring a seamless blend of public engagement and design elements, including scientific materials and signboards. Played a pivotal role in evolving the visual identity while managing meetings and field visits, compiling detailed reports and statistics for enhanced project coordination.        </div>
+                  Successfully produced over 300 pieces of content annually,
+                  driving a significant increase in social media engagement that
+                  grew the follower base to over 20,000. Supervised exhibitions,
+                  ensuring a seamless blend of public engagement and design
+                  elements, including scientific materials and signboards.
+                  Played a pivotal role in evolving the visual identity while
+                  managing meetings and field visits, compiling detailed reports
+                  and statistics for enhanced project coordination.{" "}
+                </div>
               </div>
               <div class="timeline-item ">
                 <h5 class="item-period ">2020-2021</h5>
                 <span class="item-company">Dhahran, Saudi Arabia</span>
                 <h4 class="item-title">Saudi Aramco</h4>
                 <div class="text">
-                  Expertly arranged and coordinated internal events, collaborating closely with stakeholders to guarantee successful outcomes. Innovatively designed and developed premium gifts and packaging, expanding the available options by 10%. Created distinctive visual identities for major events, including the President’s Award and the Iktifa Exhibition, while also crafting custom packaging for a prestigious book gift to King Salman bin Abdulaziz. Enhanced internal departmental sites and contributed to the COVID-19 response by designing impactful guidance posters and awareness materials.        </div>
+                  Expertly arranged and coordinated internal events,
+                  collaborating closely with stakeholders to guarantee
+                  successful outcomes. Innovatively designed and developed
+                  premium gifts and packaging, expanding the available options
+                  by 10%. Created distinctive visual identities for major
+                  events, including the President’s Award and the Iktifa
+                  Exhibition, while also crafting custom packaging for a
+                  prestigious book gift to King Salman bin Abdulaziz. Enhanced
+                  internal departmental sites and contributed to the COVID-19
+                  response by designing impactful guidance posters and awareness
+                  materials.{" "}
+                </div>
               </div>
               <div class="timeline-item">
                 <h5 class="item-period ">2018-2018</h5>
                 <span class="item-company">New Haven, United States</span>
                 <h4 class="item-title">Mascola Group</h4>
                 <div class="text">
-                  Transformed targeted website designs and marketing messages, significantly boosting user engagement and effectiveness. Developed compelling visual content for 12 brands, ensuring alignment with each client’s unique vision. Contributed to the development of wayfinding signage in New Haven through field surveys and the design of culturally inclusive icons with clear messaging.        </div>
+                  Transformed targeted website designs and marketing messages,
+                  significantly boosting user engagement and effectiveness.
+                  Developed compelling visual content for 12 brands, ensuring
+                  alignment with each client’s unique vision. Contributed to the
+                  development of wayfinding signage in New Haven through field
+                  surveys and the design of culturally inclusive icons with
+                  clear messaging.{" "}
+                </div>
               </div>
             </div>
           </div>
         </div>
-
       </div>
       <div className="container-fluid">
         <div className="row ab-row">
@@ -47,7 +78,7 @@ const Resume = () => {
           <div className="col-lg-6">
             <div class="certificate-item">
               <div class="certi-logo">
-
+                <img src={googleLogo} alt="Google" />
               </div>
               <div class="certi-content">
                 <div class="certi-title">
@@ -68,7 +99,7 @@ const Resume = () => {
           <div class="col-lg-6">
             <div class="certificate-item clearfix">
               <div class="certi-logo">
-
+                <img src={BlanoiaLogo} alt="Blanoia" />
               </div>
               <div class="certi-content">
                 <div class="certi-title">
@@ -89,7 +120,7 @@ const Resume = () => {
           <div class="col-lg-6">
             <div class="certificate-item clearfix">
               <div class="certi-logo">
-
+                <img src={docklandsLogo} alt="Docklands Media" />
               </div>
               <div class="certi-content">
                 <div class="certi-title">
@@ -110,7 +141,7 @@ const Resume = () => {
           <div class="col-lg-6">
             <div class="certificate-item clearfix">
               <div class="certi-logo">
-
+                <img src={DuisburgEssen} alt="University of Duisburg-Essen" />
               </div>
               <div class="certi-content">
                 <div class="certi-title">
@@ -131,7 +162,7 @@ const Resume = () => {
           <div class="col-lg-6">
             <div class="certificate-item clearfix">
               <div class="certi-logo">
-
+                <img src={IDEOULogo} alt="IDEO U" />
               </div>
               <div class="certi-content">
                 <div class="certi-title">
@@ -152,7 +183,7 @@ const Resume = () => {
           <div class="col-lg-6">
             <div class="certificate-item clearfix">
               <div class="certi-logo">
-
+                <img src={SaudiAramcoLogo}  alt="Saudi Aramco" />
               </div>
               <div class="certi-content">
                 <div class="certi-title">
@@ -187,79 +218,77 @@ const Resume = () => {
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-lightbulb-fill"></i>
+              <i class="bi bi-lightbulb-fill"></i>
               <h4>Innovation</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-chat-dots-fill"></i>
+              <i class="bi bi-chat-dots-fill"></i>
               <h4>Negotiation</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-grid-1x2-fill"></i>
+              <i class="bi bi-grid-1x2-fill"></i>
               <h4>Organization</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-people-fill"></i>
+              <i class="bi bi-people-fill"></i>
               <h4>Teamwork</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-clock-fill"></i>
+              <i class="bi bi-clock-fill"></i>
               <h4>Time Management</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-steam"></i>
+              <i class="bi bi-steam"></i>
               <h4>Collaboration</h4>
             </div>
           </div>
 
-
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-chat-right-text-fill"></i>
+              <i class="bi bi-chat-right-text-fill"></i>
               <h4>Storytelling</h4>
             </div>
           </div>
 
-
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-award-fill"></i>
+              <i class="bi bi-award-fill"></i>
               <h4>Leadership</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-unlock-fill"></i>
+              <i class="bi bi-unlock-fill"></i>
               <h4>Critical Thinking</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-book-half"></i>
+              <i class="bi bi-book-half"></i>
               <h4>Continuous learning</h4>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3">
             <div class="lm-info-block">
-            <i class="bi bi-eye-fill"></i>
+              <i class="bi bi-eye-fill"></i>
               <h4>Detail Oriented</h4>
             </div>
           </div>
